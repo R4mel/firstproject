d703258b33f847b4a2f55812b510ec99e827bd79
Ramel <dev_20@naver.com>
2024-05-12 13:27:40 +0900

    spring

--- src/main/java/com/example/firstproject/controller/FirstController.java
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -12,4 +12,10 @@ public class FirstController {
         model.addAttribute("username", "홍팍");
         return "greetings"; // greetings.mustache 파일 반환
     }
+
+    @GetMapping("/bye")
+    public String seeYouNext(Model model) {
+        model.addAttribute("nickname", "홍길동");
+        return "goodbye";
+    }
 }
