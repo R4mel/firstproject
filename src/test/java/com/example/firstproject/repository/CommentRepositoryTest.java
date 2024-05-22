45ebedb33f20f8be0afb7a560b504ef5467f6a7e
Ramel <dev_20@naver.com>
2024-05-22 17:39:53 +0900

    test

--- src/test/java/com/example/firstproject/repository/CommentRepositoryTest.java
+++ src/test/java/com/example/firstproject/repository/CommentRepositoryTest.java
@@ -35,10 +35,32 @@ class CommentRepositoryTest {
             // 4. 비교 및 검증
             assertEquals(expected.toString(), comments.toString(), "4번 글의 모든 댓글을 출력!");
         }
+
+        // Case 2: 1번 게시글의 모든 댓글 조회
+        {
+            Long articleId = 1L;
+            List<Comment> comments = commentRepository.findByArticleId(articleId);
+            Article article = new Article(articleId, "가가가가", "1111");
+
+            List<Comment> expected = Arrays.asList();
+
+            assertEquals(expected.toString(), comments.toString(), "1번 글은 댓글이 없음");
+        }
     }
 
     @Test
+    @DisplayName("특정 닉네임의 모든 댓글 조회")
     void findByNickname() {
+        // Case 1: "Park"의 모든 댓글 조회
+        {
+            String nickname = "Park";
+            List<Comment> comments = commentRepository.findByNickname(nickname);
+            Comment a = new Comment(1L, new Article(4L, "당신의 인생 영화는?", "댓글 고"), nickname, "굿 윌 헌팅");
+            Comment b = new Comment(4L, new Article(5L, "당신의 소울 푸드는?", "댓글 고고"), nickname, "치킨");
+            Comment c = new Comment(7L, new Article(6L, "당신의 취미는?", "댓글 고고고"), nickname, "조깅");
+            List<Comment> expected = Arrays.asList(a, b, c);
 
+            assertEquals(expected.toString(), comments.toString(), "Park의 모든 댓글 출력!");
+        }
     }
 }
